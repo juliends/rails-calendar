bc365cea29d16ca6587481a5771bea6da32c6b5e
Julien Da Silva <julien.da.silva@hotmail.com>
2017-08-03 22:58:30 +0200

    calendar without any dependency

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
     @articles = Article.all
+    @articles_by_date = @articles.group_by(&:date)
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
 end
