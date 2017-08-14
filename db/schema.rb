62ca20dd59c8a447897ca6b437a138c540723597
Julien Da Silva <julien.da.silva@hotmail.com>
2017-08-14 15:18:30 +0200

    checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803200811) do
+ActiveRecord::Schema.define(version: 20170814131636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20170803200811) do
     t.string   "title"
     t.text     "content"
     t.date     "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "published",  default: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,10 @@
 Article.destroy_all
 
 10.times do |i|
-  Article.create!(title: Faker::Hipster.sentence, content: Faker::Lorem.paragraph, date: Date.today + i)
+  Article.create!(
+    title: Faker::Hipster.sentence, 
+    content: Faker::Lorem.paragraph, 
+    date: Date.today + i, 
+    published: [true, false].sample
+  )
 end
