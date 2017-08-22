84064e94a878a0a93f558c3fd21e7f601ddd917b
Julien Da Silva <julien.da.silva@hotmail.com>
2017-08-22 16:42:22 +0200

    checkpoint

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   subject { build(:article) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a date" do
+    subject.date = nil
+    expect(subject).to_not be_valid
+  end
 end
